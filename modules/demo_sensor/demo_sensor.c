9d449354b19160b9373b3a70cb339a28115e98f4
Albert <albert.maresma.dev@gmail.com>
2026-09-23 10:54:37 +0200

    Week 2 day 2: expose sensor reading via sysfs

--- modules/demo_sensor/demo_sensor.c
+++ modules/demo_sensor/demo_sensor.c
@@ -2,13 +2,45 @@
 #include <linux/module.h>
 #include <linux/i2c.h>
 #include <linux/kernel.h>
+#include <linux/sysfs.h>
 
 #define REG_TEMP 0x00
 
+struct demo_data {
+    struct i2c_client *client;
+};
+
+static ssize_t temp_show(struct device *dev, struct device_attribute *attr, char *buf)
+{
+    struct demo_data *data = dev_get_drvdata(dev);
+    int val = i2c_smbus_read_word_data(data->client, REG_TEMP);
+
+    if (val < 0)
+        return val;
+
+    return sysfs_emit(buf, "%d\n", val);
+}
+static DEVICE_ATTR_RO(temp);
+
+static struct attribute *demo_attrs[] = {
+    &dev_attr_temp.attr,
+    NULL,
+};
+ATTRIBUTE_GROUPS(demo);
+
 static int demo_probe(struct i2c_client *client)
 {
-    int val = i2c_smbus_read_word_data(client, REG_TEMP);
+    struct demo_data *data;
+    int val;
 
+    data = devm_kzalloc(&client->dev, sizeof(*data), GFP_KERNEL);
+    if (!data)
+        return -ENOMEM;
+
+    data->client = client;
+    dev_set_drvdata(&client->dev, data);
+
+    val = i2c_smbus_read_word_data(client, REG_TEMP);
     if (val < 0) {
         dev_err(&client->dev, "read failed (%d)\n", val);
         return val;
@@ -29,12 +61,15 @@ static const struct i2c_device_id demo_id[] = {
 MODULE_DEVICE_TABLE(i2c, demo_id);
 
 static struct i2c_driver demo_driver = {
-    .driver = { .name = "demo_sensor" },
-    .probe  = demo_probe,
-    .remove = demo_remove,
+    .driver = {
+        .name = "demo_sensor",
+        .dev_groups = demo_groups,
+    },
+    .probe    = demo_probe,
+    .remove   = demo_remove,
     .id_table = demo_id,
 };
 module_i2c_driver(demo_driver);
 
 MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("Week 2: minimal I2C driver reading a simulated register");
+MODULE_DESCRIPTION("Week 2 day 2: expose sensor reading via sysfs");
